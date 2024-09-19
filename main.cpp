8f5803a3c49502289a036a6a3b362d85326d8345
Anthony Grimaldo Ramos <hernangrim.10@gmail.com>
2024-09-19 19:44:50 +0200

    actualizando main

--- main.cpp
+++ main.cpp
@@ -12,8 +12,39 @@
 
 #include "../inc/Server.hpp"
 
-int main()
+/*int main()
 {
+    Server server;
+
+    std::cout << "---- SERVER ----" << std::endl;
+    try
+    {
+        signal(SIGINT, Server::signalHandler);
+        signal(SIGQUIT, Server::signalHandler);
+        server.serverInit(42069, "hola");
+    
+    }
+    catch(const std::exception& e)
+    {
+        server.closeFds();
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << "Server Closed!" << std::endl;
+    return (0);
+}*/
+
+// Corrigiendo main
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        std::cerr << "There are not enough arguments" << std::endl;
+        return 1;
+    }
+    std::string port = argv[1];
+    std::string password = argv[2];
+
     Server server;
 
     std::cout << "---- SERVER ----" << std::endl;
