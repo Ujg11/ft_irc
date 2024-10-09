dca9691a93d0b67f6af457e29ff39bc89a255954
Oriol Jiménez Garrich <ojimenez@cbr3s6.42barcelona.com>
2024-10-09 21:26:28 +0200

    Subiendo cambios :)

--- inc/commands/Kick.hpp
+++ inc/commands/Kick.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Kick.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agrimald <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ojimenez <ojimenez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 17:52:16 by agrimald          #+#    #+#             */
-/*   Updated: 2024/09/17 17:52:17 by agrimald         ###   ########.fr       */
+/*   Updated: 2024/10/09 19:32:33 by ojimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 class Kick: public Command
 {
     public:
+        std::vector<std::string> SplitUsers(const std::string &userList);
         std::string FindReason(std::vector<std::string> &args);
         std::vector<std::string> SplitChannels(const std::string &channelList);
         void execute(Server &server, Client &c, std::vector<std::string> args);
